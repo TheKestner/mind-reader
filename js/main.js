26ecb217514d189067e350d2db67d7e47399c509
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-02-24 04:35:09 +0000

    testing array and switch for pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-const headertxt = document.getElementById('header');
-const helpertxt = document.getElementById('helper');
+const header = document.getElementById('header');
+const helper = document.getElementById('helper');
 
+let page = 0;
+
+//let headertxt = ['I can read your mind', 'string 2', 'string 3', 'string 4'];
+//    let headerOne = headertxt[0];
+//   let headerTwo = headertxt[1];
+//   let headerThree = headertxt[2];
+//  let headerFour = headertxt[3];
+//   header.innerHTML = headertxt[0];
+
+let helpertxt = ['', 'when you have your number click next', 'string 3', 'string 4'];
+    let helperOne = helpertxt[0];
+    let helperTwo = helpertxt[1];
+    let helperThree = helpertxt[2];
+    let helperFour = helpertxt[3];
+    helper.innerHTML = helpertxt[0];
+
+function updatePage() {
+    switch(page) {
+        case 0:
+        header.innerHTML = 'I can read your mind';
+        break;
+    }
+}
