9a9854c5676da1e3f87fa8b802573653a04d08b0
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-02-25 00:59:59 +0000

    fixed button issues by adding event listeners

--- js/main.js
+++ js/main.js
@@ -1,53 +1,71 @@
-const header = document.getElementById('header');
-const helper = document.getElementById('helper');
-const circlebtn = document.getElementById('circlebtn').onclick = function changeCase(){changePage(page--)};
-const squarebtn = document.getElementById('squarebtn').onclick = function changeCase(){changePage(page++)};
+let header = document.getElementById('header');
+let helper = document.getElementById('helper');
+let circlebtn = document.getElementById('circlebtn') //.onclick = function changeCase(){changePage(page--)};
+let squarebtn = document.getElementById('squarebtn') //.onclick = function changeCase(){changePage(page++)};
 
 let page = 0;
 
-    
+let resetbtn = function () {
+    if (page === 0) {
+        page++;
+    } else {
+        page = 0;
+    }
+    changePage();
+}
+
+let forward = function () {
+    page++;
+    changePage();
+}
+
+circlebtn.addEventListener('click', resetbtn);
+squarebtn.addEventListener('click', forward);
+
  function changePage() { 
     switch(page) {
         case 0:
             header.innerHTML = 'I can read your mind';
-            helper.innerHTML = '';
-            circlebtn.onclick = page++;
-            squarebtn = none;
+            helper.style.visibility = 'hidden';
+            circlebtn.innerHTML = 'GO';
+            squarebtn.style.visibility = 'hidden';
             break;
 
         case 1:
             header.innerHTML = 'Pick a number from 01 - 99';
             helper.innerHTML = 'When you have your number click next';
-            circlebtn.onclick = page--;
-            squarebtn.onclick = page++;
+            helper.style.visibility = 'visible';
+            circlebtn.innerHTML = 'Refresh';
+            squarebtn.innerHTML = 'Forward';
+            squarebtn.style.visibility = 'visible';
             break;
         
         case 2:
             header.innerHTML = 'Add both digits together to get a new number';
             helper.innerHTML = 'Ex: 14 is 1 + 4 = 5 click next to proceed';
-            circlebtn.onclick = page--;
-            squarebtn.onclick = page++;
+            circlebtn.innerHTML = 'Refresh';
+            squarebtn.innerHTML = 'Next';
             break;
         
         case 3:
             header.innerHTML = 'Subtract your new number from the original number';
             helper.innerHTML = 'Ex: 14 - 5 = 9 click next to procced';
-            circlebtn.onclick = page--;
-            squarebtn.onclick = page++;
+            circlebtn.onclick = "Refresh";
+            squarebtn.onclick = 'Next';
             break;
 
         case 4: 
             header.innerHTML = ' Need symbols list' ;
             helper.innerHTML = 'Find your new number. Note the symbol beside the number';
-            circlebtn.onclick = page--;
-            squarebtn.onclick = page++;
+            circlebtn.innerHTML = 'Refresh';
+            squarebtn.innerHTML = 'Reveal';
             break;
 
         case 5:
             header.innerHTML = 'Return symbol'
             helper.innerHTML = 'your symbol is: return symbol'
-            circlebtn.onclick = page--;
-            squarebtn.onclick = page++;
+            circlebtn.innerHTML = 'Refresh';
+            squarebtn.style.visibility = 'hidden';
             break;
 
 
